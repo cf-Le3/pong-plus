5c2b2ae2f284d8d0b5821a56b82b8be2fedccf86
cf.le3 <cf.lee0064@gmail.com>
2025-08-23 20:27:14 +0800

    Add paddles to 'Arena" scene

--- pong-but-it's-slippery/arena/arena.gd
+++ pong-but-it's-slippery/arena/arena.gd
@@ -12,6 +12,8 @@ func _ready() -> void:
 	$WallBounceLow.global_position = Vector2(viewport_w/2, viewport_h-(viewport_h-arena_h)/2)
 	$WallGoalLeft.global_position = Vector2((viewport_w-arena_w)/2, viewport_h/2)
 	$WallGoalRight.global_position = Vector2(viewport_w-(viewport_w-arena_w)/2, viewport_h/2)
+	$Paddle1.global_position = Vector2((viewport_w-arena_w)/2+32, viewport_h/2)
+	$Paddle2.global_position = Vector2(viewport_w-(viewport_w-arena_w)/2-32, viewport_h/2)
 	$Sprite2D.global_position = Vector2(viewport_w/2, viewport_h/2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
