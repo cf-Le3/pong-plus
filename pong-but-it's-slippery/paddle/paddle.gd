3a0e00da587ccf64a458809fb9724f9fbe374b2e
cf.le3 <cf.lee0064@gmail.com>
2025-08-23 23:16:51 +0800

    Implement basic movement for paddles

--- pong-but-it's-slippery/paddle/paddle.gd
+++ pong-but-it's-slippery/paddle/paddle.gd
@@ -6,4 +6,11 @@ func _ready() -> void:
 		$Sprite2D.texture = preload("res://paddle/paddle2.png")
 
 func _physics_process(delta: float) -> void:
-	pass
+	velocity = Vector2.ZERO
+	
+	if (is_player_1 && Input.is_action_pressed("up_player1")) || (!is_player_1 && Input.is_action_pressed("up_player2")):
+		velocity.y = -100
+	elif (is_player_1 && Input.is_action_pressed("down_player1")) || (!is_player_1 && Input.is_action_pressed("down_player2")):
+		velocity.y = 100
+		
+	move_and_slide()
