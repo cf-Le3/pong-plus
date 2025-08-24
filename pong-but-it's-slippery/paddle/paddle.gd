9f8321ff27121c94cbb7eba40bf2424f1bdbd93c
cf.le3 <cf.lee0064@gmail.com>
2025-08-24 09:46:44 +0800

    Update paddle sprite
    Changed paddle color and now both paddles have an identical appearance.

--- pong-but-it's-slippery/paddle/paddle.gd
+++ pong-but-it's-slippery/paddle/paddle.gd
@@ -4,8 +4,7 @@ const VELOCITY_MAX = 250
 const ACCELERATION = 5
 
 func _ready() -> void:
-	if not is_player_1:
-		$Sprite2D.texture = preload("res://paddle/paddle2.png")
+	pass
 
 func _physics_process(delta: float) -> void:
 	if (is_player_1 && Input.is_action_pressed("up_player1")) || (!is_player_1 && Input.is_action_pressed("up_player2")):
