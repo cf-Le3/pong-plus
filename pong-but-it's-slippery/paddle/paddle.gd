c25f35f1ad26571ef8c7f22aa70ef3f2ed433b77
cf.le3 <cf.lee0064@gmail.com>
2025-08-24 22:55:10 +0800

    Rename a constant
    "VELOCITY_MAX" is now "SPEED_MAX"

--- pong-but-it's-slippery/paddle/paddle.gd
+++ pong-but-it's-slippery/paddle/paddle.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 @export var is_player_1: bool
-const VELOCITY_MAX = 250
+const SPEED_MAX = 250
 const ACCELERATION = 5
 
 func _ready() -> void:
@@ -8,9 +8,9 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if (is_player_1 && Input.is_action_pressed("up_player1")) || (!is_player_1 && Input.is_action_pressed("up_player2")):
-		velocity.y = max(velocity.y - ACCELERATION, -VELOCITY_MAX)
+		velocity.y = max(velocity.y - ACCELERATION, -SPEED_MAX)
 	elif (is_player_1 && Input.is_action_pressed("down_player1")) || (!is_player_1 && Input.is_action_pressed("down_player2")):
-		velocity.y = min(velocity.y + ACCELERATION, VELOCITY_MAX)
+		velocity.y = min(velocity.y + ACCELERATION, SPEED_MAX)
 	else:
 		velocity.y = lerp(velocity.y, 0.0, 0.05)
 	var collision = move_and_collide(velocity*delta)
