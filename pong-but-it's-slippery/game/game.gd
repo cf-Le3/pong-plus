e91117fc7aaf5d09bcfc97c56c058f41e74c4bd6
cf.le3 <cf.lee0064@gmail.com>
2025-08-26 00:16:55 +0800

    Rename "arena" directory to "game"
    Performed name changes in other areas affected by the directory change.

---
+++ pong-but-it's-slippery/game/game.gd
@@ -0,0 +1,61 @@
+extends Node2D
+@export var ball_scene: PackedScene
+const ARENA_W = 1024
+const ARENA_H = 512
+const SCORE_MAX = 3
+const BALLS_MAX = 5
+var viewport_w
+var viewport_h
+var balls_spawned = 0
+var ball_textures = [load("res://ball/ball1.png"), load("res://ball/ball2.png"), load("res://ball/ball3.png")]
+var initial_angles = [45*PI/180, 60*PI/180, 75*PI/180]
+var initial_angles_offsets = [0, PI/2, PI, 3*PI/2]
+var score_player_1 = 0
+var score_player_2 = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	viewport_w = get_viewport_rect().size.x
+	viewport_h = get_viewport_rect().size.y
+	$WallBounceHigh.global_position = Vector2(viewport_w/2, (viewport_h-ARENA_H)/2)
+	$WallBounceLow.global_position = Vector2(viewport_w/2, viewport_h-(viewport_h-ARENA_H)/2)
+	$WallGoalLeft.global_position = Vector2((viewport_w-ARENA_W)/2, viewport_h/2)
+	$WallGoalRight.global_position = Vector2(viewport_w-(viewport_w-ARENA_W)/2, viewport_h/2)
+	$Paddle1.global_position = Vector2((viewport_w-ARENA_W)/2+32, viewport_h/2)
+	$Paddle2.global_position = Vector2(viewport_w-(viewport_w-ARENA_W)/2-32, viewport_h/2)
+	$BallSpawn.global_position = Vector2(viewport_w/2, viewport_h/2)
+	$Sprite2D.global_position = Vector2(viewport_w/2, viewport_h/2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if score_player_1 >= SCORE_MAX || score_player_2 >= SCORE_MAX:
+		game_over()
+	else:
+		if get_tree().get_nodes_in_group("balls").size() < 1:
+			spawn_ball()
+
+func _on_ball_timer_timeout() -> void:
+	spawn_ball()
+
+func _on_wall_goal_left_ball_escaped() -> void:
+	score_player_2 += 1
+	$HUD.update_score(score_player_2, false)
+
+func _on_wall_goal_right_ball_escaped() -> void:
+	score_player_1 += 1
+	$HUD.update_score(score_player_1, true)
+
+func spawn_ball() -> void:
+	if get_tree().get_nodes_in_group("balls").size() < BALLS_MAX:
+		var ball = ball_scene.instantiate()
+		ball.init_pos = $BallSpawn.global_position
+		ball.init_dir = initial_angles[balls_spawned%3] + initial_angles_offsets.pick_random()
+		ball.texture = ball_textures[balls_spawned%3]
+		add_child(ball)
+		balls_spawned += 1
+		$BallTimer.start()
+		
+func game_over() -> void:
+	get_tree().call_group("paddles", "queue_free")
+	get_tree().call_group("balls", "queue_free")
+	$HUD.show_game_over(score_player_1, score_player_2)
--- pong-but-it's-slippery/main/main.gd
+++ pong-but-it's-slippery/main/main.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Arena.visible = true
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
