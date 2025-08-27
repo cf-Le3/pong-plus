e5df9693797876b93c8c23923c257294fc0013a3
cf.le3 <cf.lee0064@gmail.com>
2025-08-27 23:01:19 +0800

    Add a short delay before accepting input

--- Pong Plus/title/title.gd
+++ Pong Plus/title/title.gd
@@ -4,6 +4,9 @@ signal view_help
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$WaitTimer.start()
+
+func _on_wait_timer_timeout() -> void:
 	$MultiPlayerButton.grab_focus()
 
 func _on_multi_player_button_pressed() -> void:
