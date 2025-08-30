0dd14501f19d1332ff96096db89c419a3c550ee8
cf.le3 <cf.lee0064@gmail.com>
2025-08-30 23:14:39 +0630

    Design "Credits" scene

---
+++ Pong Plus/credits/credits.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+signal close_credits
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$BackButton.grab_focus()
+
+func _on_back_button_pressed() -> void:
+	close_credits.emit()
