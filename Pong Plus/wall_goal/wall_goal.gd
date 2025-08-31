b76d3aba9ff9cea98277543434f17a99097c4ab9
cf.le3 <cf.lee0064@gmail.com>
2025-08-31 22:37:15 +0630

    Change post-goal handling logic
    The editor reported an error when the parent tries to remove multiple children in the same frame. However, as queueing removal makes it so that it only happens at the end of the frame, and the conditional to check if a new ball should be spawned happens at the end of the current frame, said conditional has been updated such that it will permit the spawning of a new ball if there are one or less balls in the tree.

--- Pong Plus/wall_goal/wall_goal.gd
+++ Pong Plus/wall_goal/wall_goal.gd
@@ -3,7 +3,6 @@ signal ball_escaped()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("balls"):
-		get_parent().remove_child(body)
 		body.queue_free()
 	ball_escaped.emit()
 	$GoalSound.play()
