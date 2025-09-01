4f53fae310803562ce559d196cfc71c35dbf1082
cf.le3 <cf.lee0064@gmail.com>
2025-09-01 23:31:04 +0630

    Update paddle.gd
    Improved readability of set_velocity_ai function. Added three new functions to calculate the paddle's velocity.

--- Pong Plus/gameplay/paddle/paddle.gd
+++ Pong Plus/gameplay/paddle/paddle.gd
@@ -10,6 +10,7 @@ func _physics_process(delta: float) -> void:
 		set_velocity_player()
 	else:
 		set_velocity_ai()
+	
 	var collision = move_and_collide(velocity*delta)
 	
 	# Slow down faster upon colliding with walls to prevent "sticking".
@@ -23,29 +24,42 @@ func _physics_process(delta: float) -> void:
 
 func set_velocity_player() -> void:
 	if (is_player_1 && Input.is_action_pressed("up_player1")) || (!is_player_1 && Input.is_action_pressed("up_player2")):
-		velocity.y = max(velocity.y - ACCELERATION, -SPEED_MAX)
+		move_up()
 	elif (is_player_1 && Input.is_action_pressed("down_player1")) || (!is_player_1 && Input.is_action_pressed("down_player2")):
-		velocity.y = min(velocity.y + ACCELERATION, SPEED_MAX)
+		move_up()
 	else:
-		velocity.y = lerp(velocity.y, 0.0, 0.05)
+		slow_to_halt()
 		
 func set_velocity_ai() -> void:
 	var balls = get_tree().get_nodes_in_group("balls")
+	
 	if balls.size() > 0:
 		var nearest_ball: CharacterBody2D = null
 		var nearest_distance: float = INF
+		
 		for b in balls:
 			if b.velocity.x > 0 && b.global_position.x < global_position.x:
 				var distance: float = global_position.x - b.global_position.x
 				if distance < nearest_distance:
 					nearest_ball = b
 					nearest_distance = distance
+
 		if nearest_ball != null:
 			if nearest_ball.global_position.y < global_position.y:
-				velocity.y = max(velocity.y - ACCELERATION, -SPEED_MAX)
+				move_up()
 			elif nearest_ball.global_position.y > global_position.y:
-				velocity.y = min(velocity.y + ACCELERATION, SPEED_MAX)
+				move_down()
 			else:
-				velocity.y = lerp(velocity.y, 0.0, 0.05)
+				slow_to_halt()
 			return
+			
+	slow_to_halt()
+	
+func move_up() -> void:
+	velocity.y = max(velocity.y - ACCELERATION, -SPEED_MAX)
+	
+func move_down() -> void:
+	velocity.y = min(velocity.y + ACCELERATION, SPEED_MAX)
+	
+func slow_to_halt() -> void:
 	velocity.y = lerp(velocity.y, 0.0, 0.05)
