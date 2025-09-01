5076c25a141a431d361afb26ed9662c2b212437a
cf.le3 <cf.lee0064@gmail.com>
2025-09-01 23:20:49 +0630

    Update ball.gd
    Improved readability of _physics_process function. Created a new function to disable paddle collision and start the Hit Timer. Added comments explaining how the ball's new velocity is calculated upon colliding with a paddle.

--- Pong Plus/gameplay/ball/ball.gd
+++ Pong Plus/gameplay/ball/ball.gd
@@ -13,23 +13,34 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity*delta)
+	
 	if collision:
 		var collider = collision.get_collider()
+		
 		if collider.is_in_group("bounce_walls"):
 			velocity.y = -1*velocity.y
 			$WallHit.play()
+			
 		elif collider.is_in_group("paddles"):
 			if can_collide_with_paddle:
 				velocity.x = -1*velocity.x
 				var collider_shape = collision.get_collider_shape()
+				
 				if velocity.y > 0 && collider_shape == collider.get_node("TopCollisionShape2D"):
 					velocity.y = -1*velocity.y
+					
 				elif velocity.y < 0 && collider_shape == collider.get_node("BottomCollisionShape2D"):
 					velocity.y = -1*velocity.y
+				
+				# Align acceleration vector with ball's velocity before adding to ball's velocity.
+				# Randomly rotate new velocity by -5 to 5 degrees.
 				velocity = (velocity + ACCELERATION.rotated(velocity.angle())).rotated(randi_range(-5, 5)*PI/180)
-				can_collide_with_paddle = false
-				$HitTimer.start()
+				disable_paddle_collision()
 				$PaddleHit.play()
+
+func disable_paddle_collision():
+	can_collide_with_paddle = false
+	$HitTimer.start()
 
 func _on_hit_timer_timeout() -> void:
 	can_collide_with_paddle = true
