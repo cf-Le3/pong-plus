77ded6cdddb872b099fabcad594959b709dd56cc
cf.le3 <cf.lee0064@gmail.com>
2025-09-02 15:45:45 +0630

    Add node "InvincibilityTimer" to "WallGoal" scene
    After a goal is scored, the affected wall is now set to be "invincible" for half a second until the new timer times out.

--- Pong Plus/gameplay/wall_goal/wall_goal.gd
+++ Pong Plus/gameplay/wall_goal/wall_goal.gd
@@ -1,8 +1,18 @@
 extends Area2D
 signal ball_escaped()
+var is_invincible := false
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("balls"):
 		body.queue_free()
 	ball_escaped.emit()
-	$GoalSound.play()
+	if not is_invincible:
+		enable_invincibility()
+		$GoalSound.play()
+
+func enable_invincibility() -> void:
+	is_invincible = true
+	$InvincibilityTimer.start()
+
+func _on_invincibility_timer_timeout() -> void:
+	is_invincible = false
