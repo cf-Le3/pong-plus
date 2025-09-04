6dd48fab16e465111b9bd014ef433b93b85ac071
cf.le3 <cf.lee0064@gmail.com>
2025-09-04 13:47:19 +0630

    Update ball_spawner.gd
    The initial angle picked is no longer random and will now cycle through the list in order.
    The statement to assign a ball's effect has also been fixed as it was incorrect.

--- Pong Plus/gameplay/ball_spawner/ball_spawner.gd
+++ Pong Plus/gameplay/ball_spawner/ball_spawner.gd
@@ -2,6 +2,7 @@ class_name BallSpawner
 extends Node2D
 signal spawned(ball: Ball)
 @export var _ball_scene: PackedScene
+var _balls_spawned := 0
 var _initial_angles := [45*PI/180, 60*PI/180, 75*PI/180]
 var _initial_angles_offsets := [0, PI/2, PI, 3*PI/2]
 var _textures : Array[Texture]
@@ -15,16 +16,17 @@ func _ready() -> void:
 		_textures = [load("res://gameplay/ball/ball1.png"), load("res://gameplay/ball/ball2.png"), load("res://gameplay/ball/ball3.png")]
 
 func spawn_ball() -> void:
+	_balls_spawned += 1
 	spawned.emit(_generate_ball())
 
 func _generate_ball() -> Ball:
 	var ball: Ball = _ball_scene.instantiate()
 	var random_index: int = randi_range(0, _NO_UNIQUE_BALLS-1)
 	ball.init_pos = $BallSpawnPoint.global_position
-	ball.init_dir = _initial_angles[random_index] + _initial_angles_offsets.pick_random()
+	ball.init_dir = _initial_angles[_balls_spawned%_NO_UNIQUE_BALLS] + _initial_angles_offsets.pick_random()
 	ball.texture = _textures[random_index]
 	if enable_magic_balls:
-		ball.effect = Ball.Effect.keys()[random_index]
+		ball.effect = Ball.Effect[Ball.Effect.keys()[random_index]]
 	else:
 		ball.effect = Ball.Effect.NORMAL
 	return ball
