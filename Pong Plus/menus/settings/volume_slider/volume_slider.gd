251c537c9e3cd8fae81aab565f6a362c89b88d18
cf.le3 <cf.lee0064@gmail.com>
2025-09-07 18:02:27 +0630

    Create new scene "VolumeSlider"

---
+++ Pong Plus/menus/settings/volume_slider/volume_slider.gd
@@ -0,0 +1,9 @@
+extends HSlider
+@export var _bus_name: String
+var _bus_index: int
+
+func _ready() -> void:
+	_bus_index = AudioServer.get_bus_index(_bus_name)
+
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(value))
