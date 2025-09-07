ca19521e1c128c0d3801600754f24a6993854d0c
cf.le3 <cf.lee0064@gmail.com>
2025-09-07 19:45:12 +0630

    Hook up settings sub-menu to title logic

--- Pong Plus/menus/title/title.gd
+++ Pong Plus/menus/title/title.gd
@@ -5,7 +5,8 @@ signal view_help
 signal view_credits
 enum ActiveMenu {
 	TITLE,
-	PLAY
+	PLAY,
+	SETTINGS
 }
 var _active_menu := ActiveMenu.TITLE
 
@@ -18,23 +19,48 @@ func _input(event: InputEvent) -> void:
 				$%SettingsButton.grab_focus()
 			elif _active_menu == ActiveMenu.PLAY:
 				$%VsPlayerButton.grab_focus()
+			elif _active_menu == ActiveMenu.SETTINGS:
+				$%GameSettingsButton.grab_focus()
 		elif event.is_action_pressed("ui_down"):
 			if _active_menu == ActiveMenu.TITLE:
 				$%CreditsButton.grab_focus()
 			elif _active_menu == ActiveMenu.PLAY:
 				$%PlayBackButton.grab_focus()
+			elif _active_menu == ActiveMenu.SETTINGS:
+				$%SettingsBackButton.grab_focus()
+
+func _switch_menu(target_menu: ActiveMenu) -> void:
+	if target_menu == ActiveMenu.TITLE:
+		if _active_menu == ActiveMenu.PLAY:
+			$%VBoxPlayButtons.visible = false
+		elif _active_menu == ActiveMenu.SETTINGS:
+			$%VBoxSettingsButtons.visible = false
+		$%VBoxTitleButtons.visible = true
+	else:
+		$%VBoxTitleButtons.visible = false
+		if target_menu == ActiveMenu.PLAY:
+			$%VBoxPlayButtons.visible = true
+		elif target_menu == ActiveMenu.SETTINGS:
+			$%VBoxSettingsButtons.visible = true
+	_active_menu = target_menu
 
 func _on_play_button_pressed() -> void:
-	if _active_menu == ActiveMenu.TITLE:
-		$%VBoxTitleButtons.visible = false
-		$%VBoxPlayButtons.visible = true
-		_active_menu = ActiveMenu.PLAY
+	_switch_menu(ActiveMenu.PLAY)
 
 func _on_play_back_button_pressed() -> void:
-	if _active_menu == ActiveMenu.PLAY:
-		$%VBoxTitleButtons.visible = true
-		$%VBoxPlayButtons.visible = false
-		_active_menu = ActiveMenu.TITLE
+	_switch_menu(ActiveMenu.TITLE)
+
+func _on_settings_button_pressed() -> void:
+	_switch_menu(ActiveMenu.SETTINGS)
+
+func _on_settings_back_button_pressed() -> void:
+	_switch_menu(ActiveMenu.TITLE)
+
+func _on_help_button_pressed() -> void:
+	view_help.emit()
+
+func _on_credits_button_pressed() -> void:
+	view_credits.emit()
 
 func _on_vs_cpu_button_pressed() -> void:
 	new_game_singleplayer.emit()
@@ -44,15 +70,6 @@ func _on_vs_player_button_pressed() -> void:
 
 func _on_endless_button_pressed() -> void:
 	pass # Replace with function body.
-
-func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_help_button_pressed() -> void:
-	view_help.emit()
-
-func _on_credits_button_pressed() -> void:
-	view_credits.emit()
 
 func set_version(version: String) -> void:
 	$%VersionLabel.text = version
