0b7c783b43dbffb0e024d324c22fa9704bb6f48b
cf.le3 <cf.lee0064@gmail.com>
2025-09-08 11:33:21 +0630

    Enable counter increment/decrement with directional UI input

--- Pong Plus/menus/settings/game_settings/game_settings.gd
+++ Pong Plus/menus/settings/game_settings/game_settings.gd
@@ -7,6 +7,19 @@ func _ready() -> void:
 	_update_counters()
 	$%PointsButtonL.grab_focus()
 
+func _input(event: InputEvent) -> void:
+	var focus_owner := get_viewport().gui_get_focus_owner()
+	if event.is_action_pressed("ui_left"):
+		if focus_owner == $%PointsButtonL:
+			$%PointsButtonL.pressed.emit()
+		elif focus_owner == $%BallsButtonL:
+			$%BallsButtonL.pressed.emit()
+	elif event.is_action_pressed("ui_right"):
+		if focus_owner == $%PointsButtonR:
+			$%PointsButtonR.pressed.emit()
+		elif focus_owner == $%BallsButtonR:
+			$%BallsButtonR.pressed.emit()
+
 func _on_points_button_l_pressed() -> void:
 	game_config.set_max_points(game_config.get_max_points()-1)
 	_update_counters()
