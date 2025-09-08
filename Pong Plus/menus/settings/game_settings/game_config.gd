fff2e65b9cdeb7b284060f8d9ab32c9542dd9d2b
cf.le3 <cf.lee0064@gmail.com>
2025-09-08 11:20:31 +0630

    Define limits for max balls and points
    The set functions have also been modified such that their corresponding attributes can never exceed the defined limits.

--- Pong Plus/menus/settings/game_settings/game_config.gd
+++ Pong Plus/menus/settings/game_settings/game_config.gd
@@ -1,15 +1,25 @@
 class_name GameConfig
 var _max_points := 5
 var _max_balls := 5
-
-func set_max_points(max_points: int) -> void:
-	_max_points = max_points
-	
-func set_max_balls(max_balls: int) -> void:
-	_max_balls = max_balls
+const _MAX_POINTS_MIN := 1
+const _MAX_POINTS_MAX := 9
+const _MAX_BALLS_MIN := 1
+const _MAX_BALLS_MAX := 9
 
 func get_max_points() -> int:
 	return _max_points
 	
 func get_max_balls() -> int:
 	return _max_balls
+
+func set_max_points(max_points: int) -> void:
+	if max_points >= _MAX_POINTS_MIN && max_points <= _MAX_POINTS_MAX:
+		_max_points = max_points
+	
+func set_max_balls(max_balls: int) -> void:
+	if max_balls >= _MAX_BALLS_MIN && max_balls <= _MAX_BALLS_MAX:
+		_max_balls = max_balls
+
+func debug_assert_correctness() -> void:
+	assert(_max_points >= _MAX_POINTS_MIN && _max_points <= _MAX_POINTS_MAX)
+	assert(_max_balls >= _MAX_BALLS_MIN && _max_balls <= _MAX_BALLS_MAX)
