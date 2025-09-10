fd7f350b0d7dbf4839f2cdb31e3ac0500d2d0d54
cf.le3 <cf.lee0064@gmail.com>
2025-09-10 11:52:16 +0630

    Update collision logic
    Paddles and balls now completely ignore each other. Instead, balls now have a new Area2D child with its own collision mask that will be triggered when a paddle enters it.

--- Pong Plus/gameplay/ball/ball.gd
+++ Pong Plus/gameplay/ball/ball.gd
@@ -9,7 +9,6 @@ var effect: Effect
 var init_pos: Vector2
 var init_dir: float
 var texture: Texture
-var can_collide_with_paddle := true
 const _INIT_SPEED := 200.0
 const _ACCELERATION := Vector2(10.0, 10.0)
 
@@ -20,34 +19,25 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(velocity*delta)
-	
 	if collision:
 		var collider := collision.get_collider()
-		
 		if collider.is_in_group("bounce_walls"):
 			velocity.y = -1*velocity.y
 			$WallHit.play()
-			
-		elif collider.is_in_group("paddles"):
-			if can_collide_with_paddle:
-				velocity.x = -1*velocity.x
-				var collider_shape := collision.get_collider_shape()
-				
-				if velocity.y > 0 && collider_shape == collider.get_node("TopCollisionShape2D"):
-					velocity.y = -1*velocity.y
-					
-				elif velocity.y < 0 && collider_shape == collider.get_node("BottomCollisionShape2D"):
-					velocity.y = -1*velocity.y
-				
-				# Align acceleration vector with ball's velocity before adding to ball's velocity.
-				# Randomly rotate new velocity by -5 to 5 degrees.
-				velocity = (velocity + _ACCELERATION.rotated(velocity.angle())).rotated(randi_range(-5, 5)*PI/180)
-				_disable_paddle_collision()
-				$PaddleHit.play()
 
-func _disable_paddle_collision():
-	can_collide_with_paddle = false
-	$HitTimer.start()
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("paddles"):
+		var paddle: Paddle = body
+		velocity.x = -1*velocity.x
+		
+		if (velocity.y > 0 && global_position.y < paddle.get_high_marker_position()) || (velocity.y < 0 && global_position.y > paddle.get_low_marker_position()):
+			velocity.y = -1*velocity.y
+			if effect == Effect.GROW:
+				paddle.grow()
+			elif effect == Effect.SHRINK:
+				paddle.shrink()
 
-func _on_hit_timer_timeout() -> void:
-	can_collide_with_paddle = true
+		# Align acceleration vector with ball's velocity before adding to ball's velocity.
+		# Randomly rotate new velocity by -5 to 5 degrees.	
+		velocity = (velocity + _ACCELERATION.rotated(velocity.angle())).rotated(randi_range(-5, 5)*PI/180)
+		$PaddleHit.play()
