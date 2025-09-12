df255e3c2a76574d452f11ccf58eb4343e936c37
cf.le3 <cf.lee0064@gmail.com>
2025-09-12 15:57:40 +0630

    Implement ball collision handling logic

--- Pong Plus/gameplay/ball/ball.gd
+++ Pong Plus/gameplay/ball/ball.gd
@@ -10,7 +10,8 @@ var init_pos: Vector2
 var init_dir: float
 var texture: Texture
 const _INIT_SPEED := 200.0
-const _ACCELERATION := Vector2(10.0, 10.0)
+const _ACCELERATION_BY_PADDLE := Vector2(10.0, 10.0)
+const _ACCELERATION_BY_BALL := Vector2(5.0, 5.0)
 
 func _ready() -> void:
 	global_position = init_pos
@@ -25,9 +26,19 @@ func _physics_process(delta: float) -> void:
 			velocity.y = -1*velocity.y
 			$WallHit.play()
 
+func enable_ball_collisions():
+	$Area2D.set_collision_mask_value(3, true)
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("paddles"):
-		var paddle: Paddle = body
+		_handle_paddle_collision(body)
+
+	elif body.is_in_group("balls") && body != self:
+		velocity.x = -1*velocity.x
+		_randomly_tilt_velocity()
+		$WallHit.play()
+
+func _handle_paddle_collision(paddle: Paddle) -> void:
 		velocity.x = -1*velocity.x
 		
 		if (velocity.y > 0 && global_position.y < paddle.get_high_marker_position()) || (velocity.y < 0 && global_position.y > paddle.get_low_marker_position()):
@@ -38,6 +49,49 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 				paddle.shrink()
 
 		# Align acceleration vector with ball's velocity before adding to ball's velocity.
-		# Randomly rotate new velocity by -5 to 5 degrees.	
-		velocity = (velocity + _ACCELERATION.rotated(velocity.angle())).rotated(randi_range(-5, 5)*PI/180)
-		$PaddleHit.play()
+		velocity = velocity + _ACCELERATION_BY_PADDLE.rotated(velocity.angle())
+		_randomly_tilt_velocity()
+		$PaddleHit.play()				
+
+func _handle_ball_collision(other_ball: Ball) -> void:
+	if !_is_matching_x_polarity_with(other_ball) && !_is_matching_y_polarity_with(other_ball):
+		if velocity.length() == other_ball.velocity.length():
+			velocity.x = -1*velocity.x
+			velocity.y = -1*velocity.y
+		elif !_is_slower_than(other_ball):
+			if abs(velocity.x) >= abs(velocity.y):
+				velocity.y = -1*velocity.y
+			else:
+				velocity.x = -1*velocity.x
+		else:
+			if abs(other_ball.velocity.x) >= abs(other_ball.velocity.y):
+				velocity.y = -1*velocity.x
+			else:
+				velocity.x = -1*velocity.x
+	elif _is_matching_x_polarity_with(other_ball) && !_is_matching_y_polarity_with(other_ball):
+		velocity.y = -1*velocity.y
+	elif !_is_matching_x_polarity_with(other_ball) && _is_matching_y_polarity_with(other_ball):
+		velocity.x = -1*velocity.x
+	elif _is_matching_x_polarity_with(other_ball) && _is_matching_y_polarity_with(other_ball):
+		var temp := Vector2(velocity.x, velocity.y)
+		velocity = Vector2(other_ball.velocity.x, other_ball.velocity.y)
+		other_ball.velocity = temp
+		
+	if _is_slower_than(other_ball):
+		# Align acceleration vector with ball's velocity before adding to ball's velocity.
+		velocity = velocity + _ACCELERATION_BY_BALL.rotated(velocity.angle())
+	_randomly_tilt_velocity()
+	$WallHit.play()
+
+func _is_matching_x_polarity_with(other_ball: Ball) -> bool:
+	return (velocity.x >= 0 && other_ball.velocity.x >= 0) || (velocity.x < 0 && other_ball.velocity.x < 0)
+
+func _is_matching_y_polarity_with(other_ball: Ball) -> bool:
+	return (velocity.y >= 0 && other_ball.velocity.y >= 0) || (velocity.y < 0 && other_ball.velocity.y < 0)
+
+func _is_slower_than(other_ball: Ball) -> bool:
+	return velocity.length() < other_ball.velocity.length()
+
+func _randomly_tilt_velocity() -> void:
+	# Randomly rotate velocity by -5 to 5 degrees.	
+	velocity = velocity.rotated(randi_range(-5, 5)*PI/180)
