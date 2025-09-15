67aa3be291d1a2dea838571dc746451710d4b937
cf.le3 <cf.lee0064@gmail.com>
2025-09-15 13:50:36 +0630

    Update game_settings.gd
    When focus switches to a radio button from a button outside the group, it will re-allocate focus to the pressed button in the group.

--- Pong Plus/menus/settings/game_settings/game_settings.gd
+++ Pong Plus/menus/settings/game_settings/game_settings.gd
@@ -2,10 +2,11 @@ class_name GameSettings
 extends CanvasLayer
 signal close_game_settings(game_config: GameConfig)
 var game_config := GameConfig.new()
+var _last_focused: Control = null
 
 func _ready() -> void:
 	_update_display()
-	
+
 	if game_config.get_difficulty() == GameConfig.Difficulty.EASY:
 		$%EasyButton.button_pressed = true
 	elif game_config.get_difficulty() == GameConfig.Difficulty.NORMAL:
@@ -19,6 +20,13 @@ func _ready() -> void:
 		$%CollisionsDisabledButton.button_pressed = true
 
 	$%PointsButtonR.grab_focus()
+	get_viewport().connect("gui_focus_changed", _on_viewport_gui_focus_changed)
+
+func _on_viewport_gui_focus_changed(node: Control):
+	if node is BaseButton && _last_focused is BaseButton:
+		if node.button_group != null && node.button_group != _last_focused.button_group:
+			node.button_group.get_pressed_button().grab_focus()
+	_last_focused = node
 
 func _input(event: InputEvent) -> void:
 	var focus_owner := get_viewport().gui_get_focus_owner()
