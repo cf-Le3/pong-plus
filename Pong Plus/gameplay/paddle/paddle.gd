38920b4e555895e8dc0203cd6424a3373d72e3b1
cf.le3 <cf.lee0064@gmail.com>
2025-09-18 16:27:34 +0630

    Refactor "Paddle"
    Made "player" variable private and created a public setter function to modify it.
    Also renamed "set_velocity" functions to "performan_action" functions as they are not setter functions.

--- Pong Plus/gameplay/paddle/paddle.gd
+++ Pong Plus/gameplay/paddle/paddle.gd
@@ -1,5 +1,6 @@
 class_name Paddle
 extends CharacterBody2D
+
 enum Player {
 	PLAYER_1,
 	PLAYER_2,
@@ -7,8 +8,10 @@ enum Player {
 	CPU_NORMAL,
 	CPU_HARD
 }
-var player: Player
+
+var _player: Player
 var _terminal_speed := 300.0
+
 const _ACCELERATION := 10.0
 const _SCALE_INCREMENT := 0.05
 const _SPEED_INCREMENT := 25.0
@@ -16,25 +19,25 @@ const _SCALE_MIN := 0.75
 const _SCALE_MAX := 1.25
 
 func _physics_process(delta: float) -> void:
-	if player == Player.PLAYER_1 || player == Player.PLAYER_2:
-		_set_velocity_player()
-	if player == Player.CPU_EASY || player == Player.CPU_NORMAL || player == Player.CPU_HARD:
-		_set_velocity_ai()
+	if _player == Player.PLAYER_1 || _player == Player.PLAYER_2:
+		_perform_action_player()
+	if _player == Player.CPU_EASY || _player == Player.CPU_NORMAL || _player == Player.CPU_HARD:
+		_perform_action_ai()
 
 	# Slow down faster upon colliding with walls to prevent "sticking".	
 	var collision := move_and_collide(velocity*delta)	
 	if collision && collision.get_collider().is_in_group("bounce_walls"):
 		velocity.y = lerp(velocity.y, 0.0, 0.2)
 
-func _set_velocity_player() -> void:
-	if (player == Player.PLAYER_1 && Input.is_action_pressed("up_player1")) || (player == Player.PLAYER_2 && Input.is_action_pressed("up_player2")):
+func _perform_action_player() -> void:
+	if (_player == Player.PLAYER_1 && Input.is_action_pressed("up_player1")) || (_player == Player.PLAYER_2 && Input.is_action_pressed("up_player2")):
 		_move_up()
-	elif (player == Player.PLAYER_1 && Input.is_action_pressed("down_player1")) || (player == Player.PLAYER_2 && Input.is_action_pressed("down_player2")):
+	elif (_player == Player.PLAYER_1 && Input.is_action_pressed("down_player1")) || (_player == Player.PLAYER_2 && Input.is_action_pressed("down_player2")):
 		_move_down()
 	else:
 		_slow_to_halt()
 		
-func _set_velocity_ai() -> void:
+func _perform_action_ai() -> void:
 	var balls := get_tree().get_nodes_in_group("balls")
 
 	if balls.size() > 0:
@@ -44,14 +47,14 @@ func _set_velocity_ai() -> void:
 		for b: Ball in balls:
 			if b.velocity.x > 0 && b.global_position.x < global_position.x:
 				var attribute: float
-				if player == Player.CPU_EASY:
+				if _player == Player.CPU_EASY:
 					# EASY: Prioritize ball closest to paddle.
 					attribute = global_position.distance_squared_to(b.global_position)
 				else:
 					# NORMAL: Prioitize ball horizontally closest to paddle.
 					attribute = global_position.x - b.global_position.x
 					# HARD: Prioritize ball that will reach paddle soonest.
-					if player == Player.CPU_HARD:
+					if _player == Player.CPU_HARD:
 						attribute = attribute / b.velocity.x
 				if attribute < priority_attribute:
 					priority_ball = b
@@ -59,7 +62,7 @@ func _set_velocity_ai() -> void:
 					
 		if priority_ball != null:
 			# HARD: Align paddle such that the ball is between the two markers.
-			if player == Player.CPU_HARD:
+			if _player == Player.CPU_HARD:
 				if priority_ball.global_position.y < get_high_marker_position():
 					_move_up()
 				elif priority_ball.global_position.y > get_low_marker_position():
@@ -85,6 +88,9 @@ func _move_down() -> void:
 	
 func _slow_to_halt() -> void:
 	velocity.y = lerp(velocity.y, 0.0, 0.05)
+
+func set_player(player: Player) -> void:
+	_player = player
 
 func get_high_marker_position() -> float:
 	return $HighMarker2D.global_position.y
