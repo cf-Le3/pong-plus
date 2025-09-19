6a5f53965b3cf6e0bd18ef58f3e55075fea5828d
cf.le3 <cf.lee0064@gmail.com>
2025-09-20 01:03:51 +0630

    Dynamically adjust focus neighbors in button bar

--- Pong Plus/gameplay/session/display.gd
+++ Pong Plus/gameplay/session/display.gd
@@ -7,6 +7,17 @@ const TEXT_CPU_WIN := "YOU LOSE"
 const TEXT_PLAYER_1_WIN := "PLAYER 1 WINS"
 const TEXT_PLAYER_2_WIN := "PLAYER 2 WINS"
 
+func _input(event: InputEvent) -> void:
+	if get_viewport().gui_get_focus_owner() == null && $%ButtonContainer.visible:
+		if $%ResumeButton.visible:
+			if event.is_action_pressed("ui_left"):
+				$%RestartButton.grab_focus()
+			elif event.is_action_pressed("ui_right"):
+				$%ExitButton.grab_focus()
+		else:
+			if event.is_action_pressed("ui_left") || event.is_action_pressed("ui_right"):
+				$%ExitButton.grab_focus()
+
 func show_ready(status := true) -> void:
 	$%LabelContainer.visible = status
 	if $%LabelContainer.visible:
@@ -36,3 +47,11 @@ func show_end(status := true, is_multiplayer := true, player_1_won := true) -> v
 		$%ResumeButton.visible = false
 	else:
 		$%ResumeButton.visible = true
+
+func _on_resume_button_visibility_changed() -> void:
+	if $%ResumeButton.visible:
+		$%RestartButton.focus_neighbor_left = NodePath($%RestartButton.get_path_to($%ResumeButton))
+		$%ExitButton.focus_neighbor_right = NodePath($%ExitButton.get_path_to($%ResumeButton))
+	else:
+		$%RestartButton.focus_neighbor_left = NodePath($%RestartButton.get_path_to($%ExitButton))
+		$%ExitButton.focus_neighbor_right = NodePath($%ExitButton.get_path_to($%RestartButton))
