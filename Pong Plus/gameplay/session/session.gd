46e977dcaa5cd9c31c57bfca97670be7d7f6d02f
cf.le3 <cf.lee0064@gmail.com>
2025-09-19 23:23:30 +0630

    Update session.gd
    Consolidate _create_new_game and _start_game functions.

--- Pong Plus/gameplay/session/session.gd
+++ Pong Plus/gameplay/session/session.gd
@@ -17,18 +17,14 @@ const TEXT_PLAYER_1_WIN := "PLAYER 1 WINS"
 const TEXT_PLAYER_2_WIN := "PLAYER 2 WINS"
 
 func _ready() -> void:
-	_create_new_game()
 	_start_game()
-
-func _create_new_game() -> void:
+	
+func _start_game() -> void:
 	_game = _game_scene.instantiate()
 	_game.is_multiplayer = is_multiplayer
 	_game.game_config = game_config
 	_game.connect("game_ended", _on_game_ended)
 	add_child(_game)
-
-func _start_game() -> void:
-	assert(_game != null)
 	_show_ready(true)
 	$StartGameTimer.start()
 	$StartGameSound.play()
