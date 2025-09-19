3f022f7bcd0f5930bf98f3efea1b66be04d0bed9
cf.le3 <cf.lee0064@gmail.com>
2025-09-19 18:21:20 +0630

    Add UI elements to Session scene
    Message label and button bar added. Functions to display visibility of these elements have also been defined.

--- Pong Plus/gameplay/session/session.gd
+++ Pong Plus/gameplay/session/session.gd
@@ -9,6 +9,13 @@ var is_multiplayer := false
 var game_config := GameConfig.new()
 var _game: Game
 
+const TEXT_READY := "READY"
+const TEXT_PAUSED := "PAUSED"
+const TEXT_PLAYER_WIN := "YOU WIN"
+const TEXT_CPU_WIN := "YOU LOSE"
+const TEXT_PLAYER_1_WIN := "PLAYER 1 WINS"
+const TEXT_PLAYER_2_WIN := "PLAYER 2 WINS"
+
 func _ready() -> void:
 	_create_new_game()
 
@@ -19,5 +26,35 @@ func _create_new_game() -> void:
 	_game.connect("game_ended", _on_game_ended)
 	add_child(_game)
 
+func _show_ready(status: bool) -> void:
+	$%LabelContainer.visible = status
+	if $%LabelContainer.visible:
+		$%Label.text = TEXT_READY
+		
+func _show_pause(status: bool) -> void:
+	$%LabelContainer.visible = status
+	if $%LabelContainer.visible:
+		$%Label.text = TEXT_PAUSED
+	$%ButtonContainer.visible = status
+	
+func _show_end(status: bool, player_1_won: bool = true) -> void:
+	$%LabelContainer.visible = status
+	if $%LabelContainer.visible:
+		if is_multiplayer:
+			if player_1_won:
+				$%Label.text = TEXT_PLAYER_1_WIN
+			else:
+				$%Label.text = TEXT_PLAYER_2_WIN
+		else:
+			if player_1_won:
+				$%Label.text = TEXT_PLAYER_WIN
+			else:
+				$%Label.text = TEXT_CPU_WIN
+	$%ButtonContainer.visible = status
+	if %ButtonContainer.visible:
+		$%ResumeButton.visible = false
+	else:
+		$%ResumeButton.visible = true
+	
 func _on_game_ended() -> void:
 	session_closed.emit()
