1ccdb0d097e30f2d709bc0645ca3207bcc1484f8
cf.le3 <cf.lee0064@gmail.com>
2025-09-24 23:35:40 +0630

    Update survival_game.gd
    Removed override of _do_staff_after_ball_escaped.

--- Pong Plus/gameplay/game/survival_game.gd
+++ Pong Plus/gameplay/game/survival_game.gd
@@ -25,18 +25,15 @@ func _on_second_timer_timeout() -> void:
 	_time_elapsed += 1
 
 func _on_wall_goal_right_ball_escaped(is_invincible: bool) -> void:
-	_ball_escaped_common_logic(is_invincible)
+	_respond_to_ball_escaped(is_invincible)
 
 func _on_wall_goal_left_ball_escaped(is_invincible: bool) -> void:
-	_ball_escaped_common_logic(is_invincible)
+	_respond_to_ball_escaped(is_invincible)
 
-func _ball_escaped_common_logic(is_invincible: bool) -> void:
+func _respond_to_ball_escaped(is_invincible: bool) -> void:
 	if not is_invincible && _health > 0:
 		_health -= 1
 		$HUD.update_survival_health(_health)
-	_do_stuff_after_ball_escaped()
-
-func _do_stuff_after_ball_escaped() -> void:
 	if _health == 0:
 		_game_over()
 	elif get_tree().get_nodes_in_group("balls").size() <= 1:
