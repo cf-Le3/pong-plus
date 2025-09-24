3b1565db9f7471058fde028daa5086e0afe930ef
cf.le3 <cf.lee0064@gmail.com>
2025-09-24 18:25:51 +0630

    Update survival_game.gd
    Added a variable that keeps a reference to a timer node that is initialized at runtime. This timer fires every second and increments the time elapsed when it does. The timer is stopped when the game ends.

--- Pong Plus/gameplay/game/survival_game.gd
+++ Pong Plus/gameplay/game/survival_game.gd
@@ -3,7 +3,27 @@ extends Game
 
 const MAX_HEALTH := 5
 
+# Nodes
+var secondTimer: Timer
+
 # Game state
 var _health := MAX_HEALTH
 var _score := 0
 var _time_elapsed := 0
+
+func _ready() -> void:
+	super()
+	secondTimer = Timer.new()
+	secondTimer.connect("timeout", _on_second_timer_timeout)
+
+func begin() -> void:
+	secondTimer.start()
+	super()
+
+func _on_second_timer_timeout() -> void:
+	_time_elapsed += 1
+
+func _game_over() -> void:
+	secondTimer.stop()
+	print("DEBUG | Time elapsed: " + str(_time_elapsed))
+	super()
