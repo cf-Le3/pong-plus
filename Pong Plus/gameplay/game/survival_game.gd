0a130aaafed957188d33b1f0aab6194264414a30
cf.le3 <cf.lee0064@gmail.com>
2025-09-24 18:28:05 +0630

    Update survival_game.gd
    Callbacks for responding to the ball leaving the playing field via the walls have been overridden. They now deduct from the player's health.

--- Pong Plus/gameplay/game/survival_game.gd
+++ Pong Plus/gameplay/game/survival_game.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 	super()
 	secondTimer = Timer.new()
 	secondTimer.connect("timeout", _on_second_timer_timeout)
+	_debug_print_health()
 
 func begin() -> void:
 	secondTimer.start()
@@ -22,6 +23,29 @@ func begin() -> void:
 
 func _on_second_timer_timeout() -> void:
 	_time_elapsed += 1
+
+func _on_wall_goal_right_ball_escaped(can_score: bool) -> void:
+	print("DEBUG | Ball escaped via right wall!")
+	_ball_escaped_common_logic()
+
+func _on_wall_goal_left_ball_escaped(can_score: bool) -> void:
+	print("DEBUG | Ball escaped via left wall!")
+	_ball_escaped_common_logic()
+
+func _ball_escaped_common_logic() -> void:
+	if _health > 0:
+		_health -= 1
+	_debug_print_health()
+	_do_stuff_after_ball_escaped()
+
+func _do_stuff_after_ball_escaped() -> void:
+	if _health == 0:
+		_game_over()
+	elif get_tree().get_nodes_in_group("balls").size() <= 1:
+		_ball_spawner.spawn_ball()
+
+func _debug_print_health() -> void:
+	print("DEBUG | HP: " + str(_health))
 
 func _game_over() -> void:
 	secondTimer.stop()
