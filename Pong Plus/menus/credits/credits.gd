9205d058853e00d7c0bc449dc79447f399f3c57d
cf.le3 <cf.lee0064@gmail.com>
2025-10-06 13:34:18 +0630

    Update credits.gd
    Implemented keyboard scrolling functionality.

--- Pong Plus/menus/credits/credits.gd
+++ Pong Plus/menus/credits/credits.gd
@@ -3,8 +3,13 @@ extends CanvasLayer
 
 signal closed
 
-func _ready() -> void:
-	$BackButton.grab_focus()
+func _input(event: InputEvent) -> void:
+	if get_viewport().gui_get_focus_owner() == null:
+		$%BackButton.grab_focus()
+	if event.is_action("ui_up"):
+		$%ScrollContainer.set_v_scroll($%ScrollContainer.get_v_scroll()-10)
+	elif event.is_action("ui_down"):
+		$%ScrollContainer.set_v_scroll($%ScrollContainer.get_v_scroll()+10)
 
 func _on_back_button_pressed() -> void:
 	closed.emit()
